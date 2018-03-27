d44a6ef8030ae73214a0f9700bf8e59c304a1705
Atıf DAĞ <atifdag@msn.com>
2018-03-27 23:29:39 +0300

    -

--- src/YedaKit/Presentation/YedaKit.WebApi/Startup.cs
+++ src/YedaKit/Presentation/YedaKit.WebApi/Startup.cs
@@ -24,6 +24,7 @@ namespace YedaKit.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
